c1145175e87afa7ba5cc30e45e7a00563c5c9594
lftnnisa <liftaah@gmail.com>
2023-03-02 08:27:08 +0700

    change color

--- lib/main.dart
+++ lib/main.dart
@@ -96,7 +96,7 @@ class _MyHomePageState extends State<MyHomePage> {
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             const Text(
-              'Orderan anda sebanyak:',
+              'Orderan anda sebanyak ::',
             ),
             Text(
               '$_counter',
